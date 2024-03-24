73e480d2585c669139ec345425f1608c84521f52
ryanimay <ryanimay840121@gmail.com>
2024-03-24 10:38:17 +0800

    fix message svg icon error

--- src/components/header/LoginHeader.vue
+++ src/components/header/LoginHeader.vue
@@ -6,7 +6,7 @@
 #header-container{
     width: 100%;
     height: 100vh;
-    background-image:url('../../assets/pexels-laura-tancredi-7078666.jpg');
+    background-image:url('@/assets/index-background.jpg');
     background-size: cover;
 }
 </style>
--- src/components/tool/AlterMessage.vue
+++ src/components/tool/AlterMessage.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="message" :class="messageClass" v-if="visible">
-        <span id="icon" v-html="svgContent"></span>
+        <img :src="svgContent" id="icon" alt=""/>
         <span class="text">{{ text }}</span>
     </div>
 </template>
 
 <script setup>
 import { ref, defineProps, onMounted } from 'vue';
-
+import successIcon from '@/assets/icon/success.svg';
+import errorIcon from '@/assets/icon/error.svg';
+import warnIcon from '@/assets/icon/warn.svg';
 const props = defineProps({
     text: {
         type: String,
@@ -26,15 +28,15 @@ const svgContent = ref('');
 onMounted(() => {
     switch (props.type) {
         case 'warn':
-            svgContent.value = require('@/assets/icon/warn.svg').default;
+            svgContent.value = warnIcon;
             messageClass.value = 'warn-message';
             break;
         case 'error':
-            svgContent.value = require('@/assets/icon/error.svg').default;
+            svgContent.value = errorIcon;
             messageClass.value = 'error-message';
             break;
         case 'success':
-            svgContent.value = require('@/assets/icon/success.svg').default;
+            svgContent.value = successIcon;
             messageClass.value = 'success-message';
             break;
         default:
@@ -56,30 +58,26 @@ onMounted(() => {
     top: 60px;
     transform: translate(-50%);
     padding: 12px;
-    background-color: white;
+    background-color: rgb(255, 255, 255);
     border: 1px solid rgba(0, 0, 0, 0.1);
     display: flex;
     align-items: center;
     justify-content: center;
-    animation: downed 100ms ease;
+    animation: downed 100ms ease, fadeOut 100ms ease 2900ms forwards;
     border-radius: 4px;
     box-sizing: border-box;
 }
 
 .warn-message {
-    color: yellow;
+    color: rgb(255, 215, 0);
 }
 
 .error-message {
-    color: red;
+    color: rgb(230, 0, 0);
 }
 
 .success-message {
-    color: green;
-}
-
-#icon svg path {
-    fill: red;
+    color: rgb(0, 128, 0);
 }
 
 @keyframes downed {
@@ -91,6 +89,15 @@ onMounted(() => {
     100% {
         opacity: 1;
         top: 60px;
+    }
+}
+
+@keyframes fadeOut {
+    from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
     }
 }
 
