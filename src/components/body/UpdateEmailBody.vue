35bbbf541b30aae0abc16f7a431bd07127b89ffa
Ryan <ryan@esound.com.tw>
2024-03-25 12:00:31 +0800

    add updateEmailPage

--- src/components/body/UpdateEmailBody.vue
+++ src/components/body/UpdateEmailBody.vue
@@ -1,27 +1,18 @@
 <template>
     <div id="body-container" v-loading.fullscreen.lock="loading" element-loading-background="rgba(0, 0, 0, 0.5)">
         <el-row style="width: 100%;">
-            <el-col :span="11">
+            <el-col>
                 <h2>UpdateEmail</h2>
-            </el-col>
-            <el-col :span="13">
-                <div id="messageFrame">
-                    <p v-if="message" id="message">{{ message }}</p>
-                    <p v-else></p>
-                </div>
             </el-col>
         </el-row>
         <el-form :model="formData" label-position="left" label-width="auto" size="large">
-            <el-form-item label="Username:">
-                <el-input v-model="formData.username" class="input-area" />
-            </el-form-item>
             <el-form-item label="Email:">
                 <el-input v-model="formData.email" class="input-area" />
             </el-form-item>
             <el-form-item>
-                <el-row style="width: 100%;">
+                <el-row id="btnFrame">
                     <el-col :span="4">
-                        <el-button type="primary" @click="lastPage" class="btn">Cancel</el-button>
+                        <el-button type="info" @click="lastPage" class="btn">Cancel</el-button>
                     </el-col>
                     <el-col :span="2"></el-col>
                     <el-col :span="18">
@@ -36,32 +27,31 @@
 <script setup>
 import config from '@/config/RouterPath';
 import { reactive, ref, getCurrentInstance } from 'vue';
-import { useRouter } from 'vue-router';
+const user = JSON.parse(localStorage.getItem('user'));
 const loading = ref(false);
 const formData = reactive({
-    username: '',
+    id: user.id,
     email: ''
 })
 const { proxy } = getCurrentInstance();//獲取全局組件
-const message = ref(proxy.$route.query.message);
-const router = useRouter();
+
 const lastPage = () => {
-
-
-
-    router.go(-1);
+    proxy.$router.push({ name: 'login' });
 }
 
 const resetPassword = async () => {
     try {
         loading.value = true;
-        const response = await proxy.$axios.put(config.api.client.resetPassword.path, formData);
+        const response = await proxy.$axios.put(config.api.client.update.path, formData);
         if (response.data.code != 200) {
-            message.value = response.data.data;
+            proxy.$msg.error(response.data.data);
         } else {
-            // proxy.$router.push({ name: 'home' });
+            localStorage.setItem('user', JSON.stringify(response.data.data));
+            proxy.$msg.success('Update success');
+            proxy.$router.push({ name: 'home' });
         }
     } catch (error) {
+        proxy.$msg.error('Unknown Error');
         console.error('API request failed:', error);
     } finally {
         loading.value = false;
@@ -78,9 +68,9 @@ const resetPassword = async () => {
     padding: 10px 30px 10px 30px;
     border-radius: 15px;
     width: 346px;
-    height: 236px;
+    height: 180px;
     margin-left: -203px;
-    margin-top: -128px;
+    margin-top: -100px;
 }
 
 .btn {
@@ -125,5 +115,10 @@ const resetPassword = async () => {
     display: flex;
     align-items: center;
     justify-content: center;
+}
+
+#btnFrame{
+    width: 100%;
+    margin-top: 10px;
 }
 </style>
