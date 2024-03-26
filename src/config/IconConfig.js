0e75de52f16b1a7d211c3f8e68b3c5e806ea3d73
Ryan <ryan@esound.com.tw>
2024-03-26 16:51:14 +0800

    remove redundant dependencies

--- src/config/IconConfig.js
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {  } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add();
-
-export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import store from './config/Store'
 import router from './config/RouterConfig'
 import axios from './config/Axios.js';
-import validate from './config/ValidateConfig';
-import icons from './config/IconConfig';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -13,10 +11,6 @@ import msg from './config/AlterConfig.js'
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$msg = msg;
-app.component('VField', validate.Field);
-app.component('VForm', validate.Form);
-app.component('ErrorMessage', validate.ErrorMessage);
-app.component('font-awesome-icon', icons);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
