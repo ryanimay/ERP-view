13f2e701bf3b8ff006f3882ac5ff1c78c852df5c
ryanimay <ryanimay840121@gmail.com>
2024-04-07 20:16:34 +0800

    unfinish clientBody

--- src/components/body/ClinetBody.vue
+++ src/components/body/ClinetBody.vue
@@ -1,5 +1,15 @@
 <template>
     <el-main class="homeBodyContainer">
-        <div>clientbody</div>
+        <el-container>
+            <el-header>
+                <div>header</div>
+            </el-header>
+            <el-main>
+                <div>main</div>
+            </el-main>
+            <el-footer>
+                <div>footer</div>
+            </el-footer>
+        </el-container>
     </el-main>
 </template>
