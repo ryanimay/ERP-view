ad7da2d259a2b453f4e7c0700756a353a98eba01
Ryan <ryan@esound.com.tw>
2024-08-21 17:22:23 +0800

    update sortablejs autoScroll

--- src/config/sortable.js
+++ src/config/sortable.js
@@ -1,4 +1,4 @@
-import Sortable from 'sortablejs'
+import Sortable from 'sortablejs';
 
 let sortableInstance = null
 export function setSort(tableData, element) {
@@ -12,9 +12,17 @@ export function setSort(tableData, element) {
         sortableInstance = null
     }
     sortableInstance = new Sortable(el, {
+        animation: 150,//過渡動畫
         sort: true,
-        ghostClass: 'sortable-ghost',
+        ghostClass: 'sortable-ghost',//被選取樣式
+        forceFallback: true,//禁用html，下面autoScroll配置才會生效
+        scrollSensitivity: 100,//觸發滾動距離
+        scrollSpeed: 15,//滾動速度
+        onStart: () => {
+            document.body.style.userSelect = 'none'; // 禁用文本選擇，避免拖曳反白
+        },
         onEnd: (e) => {
+            document.body.style.userSelect = ''; // 恢復文本選擇
             const targetRow = tableData.splice(e.oldIndex, 1)[0]
             tableData.splice(e.newIndex, 0, targetRow)
         },
